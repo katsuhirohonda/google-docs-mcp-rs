754ab86a8d9ab7d23da7e423651627164ab9c6d6
Katsuhiro Honda <freewave3@gmail.com>
2026-01-04 20:36:46 +0900

    chore(constants): remove unused CHARACTER_LIMIT constant

--- src/constants.rs
+++ src/constants.rs
@@ -7,8 +7,5 @@ pub const GOOGLE_TOKEN_URL: &str = "https://oauth2.googleapis.com/token";
 /// Google Docs API scope
 pub const GOOGLE_DOCS_SCOPE: &str = "https://www.googleapis.com/auth/documents";
 
-/// Maximum character limit for responses
-pub const CHARACTER_LIMIT: usize = 25_000;
-
 /// JWT expiration time in seconds (1 hour)
 pub const JWT_EXPIRATION_SECS: i64 = 3600;
